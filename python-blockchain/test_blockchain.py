fc1a7ec8cdd59a8ce52b11320501b4bd028f261d
Sudeep Hazra <sudeephazra@gmail.com>
2018-10-27 21:20:42 +0530

    Adding test method
    Adding test method

--- python-blockchain/test_blockchain.py
+++ python-blockchain/test_blockchain.py
@@ -2,29 +2,33 @@ import Block
 import json
 import datetime
 
-block = Block.Block()
-# Create the blockchain and add the genesis block
-blockchain = [block.create_genesis_block()]
-previous_block = blockchain[0]
-num_of_blocks_to_add = 5
 
-# Add blocks to the chain
-for i in range(0, num_of_blocks_to_add):
-    block_to_add = block.next_block(previous_block)
-    blockchain.append(block_to_add)
-    previous_block = block_to_add
+def test_blockchain_python():
+    block = Block.Block()
+    # Create the blockchain and add the genesis block
+    blockchain = [block.create_genesis_block()]
+    previous_block = blockchain[0]
+    num_of_blocks_to_add = 5
 
-# Prints the entire blockchain in JSON form
-for i in range(0, blockchain.__len__()):
-    print(json.dumps(blockchain[i].__dict__))
+    # Add blocks to the chain
+    for i in range(0, num_of_blocks_to_add):
+        block_to_add = block.next_block(previous_block)
+        blockchain.append(block_to_add)
+        previous_block = block_to_add
 
-print("########################################")
-# Printing the original block
-print(json.dumps(blockchain[2].__dict__))
-# Tampering with the block data
-blockchain[2].data = "Modified"
-# Re-hashing the block
-blockchain[2].hash = Block.Block.hash_block(blockchain[2])
-# Printing the tampered block
-print(json.dumps(blockchain[2].__dict__))
-print("########################################")
+    # Prints the entire blockchain in JSON form
+    for i in range(0, blockchain.__len__()):
+        print(json.dumps(blockchain[i].__dict__))
+
+    print("########################################")
+    # Printing the original block
+    print(json.dumps(blockchain[2].__dict__))
+    # Tampering with the block data
+    blockchain[2].data = "Modified"
+    # Re-hashing the block
+    blockchain[2].hash = Block.Block.hash_block(blockchain[2])
+    # Printing the tampered block
+    print(json.dumps(blockchain[2].__dict__))
+    print("########################################")
+    
+test_blockchain_python()
